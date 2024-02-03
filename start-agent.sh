945ec010f7440b29a8d5bcb274a7c1da40bfa558
redgrist <46484144+redgrist@users.noreply.github.com>
2024-02-03 21:44:11 +0100

    Update start-agent.sh

--- start-agent.sh
+++ start-agent.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /usr/local/jenkins-service
 curl -sO http://192.168.22.114:8080/jnlpJars/agent.jar
-java -jar agent.jar -url http://192.168.22.114:8080/ -secret 925bad97e72d61f6a996489018ba2cef6c0d0dab1a53dd45f8248dc3b617d9cb -name arm64 -workDir "/home/jenkins"
+java -jar agent.jar -url http://192.168.22.114:8080/ -secret 925bad97e7---------------------------53dd45f8248dc3b617d9cb -name arm64 -workDir "/home/jenkins"
 exit 0
